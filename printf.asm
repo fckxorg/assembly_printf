17d212f07250ae4c368e85e0e238cbc6f62f58eb
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-03-16 20:43:20 +0300

    added %s format

--- printf.asm
+++ printf.asm
@@ -7,9 +7,8 @@
 		section .text
 
 _start: 	mov	rsi, message
-		push 	0x64
-		push	0x65
 		push	0x64
+		push 	greater
 		
 		
 		call 	printf
@@ -55,6 +54,32 @@ putc:		mov 	rax, 1 ; syscall write
 		syscall
 		ret
 
+putline:	mov	rdx, rsi
+		call 	strlen
+		mov 	rax, 1
+		mov	rdi, 1
+		syscall
+		ret
+		
+
+;--------------------------------------------------
+; Finds zero-terminated string length.
+; Enter: RDX - pointer to string
+; Uses: RAX, RDX
+; Output: RDX - length
+;---------------------------------------------------
+strlen:		mov 	rax, rdx
+lenCycle:	cmp 	byte [rdx], 0
+		je	lenCycleEnd
+		inc	rdx			
+		jmp	lenCycle
+lenCycleEnd:	sub rdx, rax
+		ret
+;-----------------------------------------------------
+; Parses printf format specifiers and calls handlers
+; for each specifier
+; Enter: RSI - pointer to format specifier
+;----------------------------------------------------
 formatParse:	inc 	rsi
 		cmp	byte [rsi], 0x25 ; checking if we need to output %
 		jne 	checkChar
@@ -62,7 +87,7 @@ formatParse:	inc 	rsi
 		jmp 	parseEnd
 
 checkChar:	cmp	byte [rsi], 0x63 ; cheking if need to output char
-		jne 	parseEnd
+		jne 	checkStr
 		
 		push 	rsi	
 		mov 	rsi, rbp
@@ -72,9 +97,19 @@ checkChar:	cmp	byte [rsi], 0x63 ; cheking if need to output char
 		pop 	rsi
 		jmp 	parseEnd
 
+checkStr:	cmp	byte [rsi], 0x73
+		jne	parseEnd
+		
+		push	rsi
+		mov 	rsi, [rbp]
+		add	rbp, 8
+		call 	putline
+		pop 	rsi
+		jmp 	parseEnd
+		
 parseEnd:	ret
 
 
 		section .data
-message:	db 	"Hello, %c%c%c!", 10, 0
-	
+message:	db 	"Hello,%s %% %c!", 10, 0
+greater:	db	"I am the test string", 0	
