fb0cafb97fa7f9f94f6814f803687ca507a6afca
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-03-17 14:59:14 +0300

    now can print unsigned integers

--- printf.asm
+++ printf.asm
@@ -1,5 +1,5 @@
 ; ----------------------------------------------------------
-; NASM test program
+; NASM printf implementation (c) fckxorg, 2020
 ;-----------------------------------------------------------
 
 		global _start
@@ -9,7 +9,7 @@
 _start: 	mov	rsi, message
 		push	0x64
 		push 	greater
-		
+		push	15
 		
 		call 	printf
 		
@@ -54,11 +54,45 @@ putc:		mov 	rax, 1 ; syscall write
 		syscall
 		ret
 
+;-------------------------------------------------------
+; Puts string to stdout
+; Enter: RSI - pointer to zero-terminated string
+; Uses:	RDX, RAX, RDI, RSI
+;-------------------------------------------------------
 putline:	mov	rdx, rsi
 		call 	strlen
+		mov 	rdx, rax
 		mov 	rax, 1
 		mov	rdi, 1
 		syscall
+		ret
+
+;------------------------------------------------------------
+; Outputs value of RAX to stdout as integer in number system 
+; defined in RCX. 
+; Eneter: RAX - number, RCX - number system
+; Uses: RDX, RBX, RSI, RDI, R0
+;------------------------------------------------------------
+itoa:		mov 	rbx, charTable
+		mov 	r8, 31	
+renomCycle:	cmp	rax, 0
+		je 	renomCycleEnd
+		xor	rdx, rdx
+		div	rcx
+		push	rax
+		mov 	rax, rdx	
+		xlat
+		mov	[revItoaBuff + r8], al
+		pop 	rax
+		dec	r8
+		xor	rdx, rdx
+		jmp 	renomCycle
+		
+		
+renomCycleEnd:	mov 	rsi, revItoaBuff
+		add 	rsi, r8
+		add 	rsi, 1
+		call 	putline
 		ret
 		
 
@@ -69,11 +103,11 @@ putline:	mov	rdx, rsi
 ; Output: RDX - length
 ;---------------------------------------------------
 strlen:		mov 	rax, rdx
-lenCycle:	cmp 	byte [rdx], 0
+lenCycle:	cmp 	byte [rax], 0
 		je	lenCycleEnd
-		inc	rdx			
+		inc	rax			
 		jmp	lenCycle
-lenCycleEnd:	sub rdx, rax
+lenCycleEnd:	sub rax, rdx
 		ret
 ;-----------------------------------------------------
 ; Parses printf format specifiers and calls handlers
@@ -98,7 +132,7 @@ checkChar:	cmp	byte [rsi], 0x63 ; cheking if need to output char
 		jmp 	parseEnd
 
 checkStr:	cmp	byte [rsi], 0x73
-		jne	parseEnd
+		jne	checkUint
 		
 		push	rsi
 		mov 	rsi, [rbp]
@@ -106,10 +140,25 @@ checkStr:	cmp	byte [rsi], 0x73
 		call 	putline
 		pop 	rsi
 		jmp 	parseEnd
+
+checkUint:	cmp 	byte [rsi], 0x75
+		jne	parseEnd
+		mov	rax, [rbp]
+		add 	rbp, 8
+		mov 	rcx, 10
+		push	rsi
+		call 	itoa
+		pop	rsi
+		jmp	parseEnd
 		
 parseEnd:	ret
 
 
 		section .data
-message:	db 	"Hello,%s %% %c!", 10, 0
+message:	db 	"Hello,%u %s %% %c!", 10, 0
 greater:	db	"I am the test string", 0	
+charTable:	db	"0123456789abcdef"
+
+		section	.bss
+itoaBuff:	resb	32
+revItoaBuff:	resb	32
